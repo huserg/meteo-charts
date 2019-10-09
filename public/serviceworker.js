e47e5c672cf6728345bfb4ff8c6c6bd55ecc58e7
Gaétan Huser <huser.gaetan@hotmail.com>
2019-10-09 13:47:09 +0200

    update icons

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -3,14 +3,9 @@ var filesToCache = [
     '/offline',
     '/css/app.css',
     '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/images/icons/icon-96.png',
+    '/images/icons/icon-192.png',
+    '/images/icons/icon-512.png',
 ];
 
 // Cache on install
